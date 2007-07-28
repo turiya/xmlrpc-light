c73ff7451cb3263996bb60c97ec4c24b776b5b54
dave.benjamin <dave.benjamin@49675f9e-a734-0410-ba66-8ba39d4a960b>
2007-07-28 03:09:33 +0000

    Ported everything to Netcgi2, added CGI server and generic server base class

--- trunk/XmlRpcServer.ml
+++ trunk/XmlRpcServer.ml
@@ -8,9 +8,56 @@ let invalid_params () =
     (XmlRpc.Error
        (-32602, "server error. invalid method parameters"))
 
-class netplex ?(parallelizer=Netplex_mp.mp()) ?(handler="xmlrpc") () =
+let system_get_capabilities _ =
+  `Struct
+    [
+      "system.multicall",
+      `Struct ["specUrl", `String "http://www.xmlrpc.com/discuss/msgReader$1208";
+               "specVersion", `Int 1];
+      "xmlrpc",
+      `Struct ["specUrl", `String "http://www.xmlrpc.com/spec"; "specVersion", `Int 1];
+      "faults_interop",
+      `Struct ["specUrl", `String "http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php";
+               "specVersion", `Int 20010516];
+    ]
+
+let system_list_methods methods _ =
+  let names = ref [] in
+  Hashtbl.iter
+    (fun n _ -> names := `String n :: !names)
+    methods;
+  `Array (List.sort compare !names)
+
+let system_multicall methods = function
+  | [`Array calls] ->
+      `Array
+        (List.map
+           (function
+              | `Struct ["methodName", `String name;
+                         "params", `Array params]
+              | `Struct ["params", `Array params;
+                         "methodName", `String name] ->
+                  (try `Array [(try Hashtbl.find methods name
+                                with Not_found -> invalid_method name)
+                                 params]
+                   with
+                     | XmlRpc.Error (code, string) ->
+                         `Struct ["faultCode", `Int code;
+                                  "faultString", `String string]
+                     | e ->
+                         `Struct ["faultCode", `Int (-32500);
+                                  "faultString",
+                                  `String
+                                    ("application error. "
+                                     ^ Printexc.to_string e)])
+              | _ -> invalid_params ())
+           calls)
+  | _ -> invalid_params ()
+
+class base =
 object (self)
-  val methods = Hashtbl.create 0
+  val methods =
+    (Hashtbl.create 0 : (string, XmlRpc.value list -> XmlRpc.value) Hashtbl.t)
 
   val mutable base64_encode = fun s -> XmlRpcBase64.str_encode s
   val mutable base64_decode = fun s -> XmlRpcBase64.str_decode s
@@ -31,56 +78,16 @@ object (self)
     Hashtbl.remove methods name
 
   initializer
-    self#register "system.getCapabilities"
-      (fun _ ->
-         `Struct
-           [
-             "system.multicall",
-             `Struct ["specUrl", `String "http://www.xmlrpc.com/discuss/msgReader$1208";
-             "specVersion", `Int 1];
-             "xmlrpc",
-             `Struct ["specUrl", `String "http://www.xmlrpc.com/spec"; "specVersion", `Int 1];
-             "faults_interop",
-             `Struct ["specUrl", `String "http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php";
-                      "specVersion", `Int 20010516];
-           ]);
+    self#register "system.getCapabilities" system_get_capabilities;
+    self#register "system.listMethods" (system_list_methods methods);
+    self#register "system.multicall" (system_multicall methods);
+end
 
-    self#register "system.listMethods"
-      (fun _ ->
-         let names = ref [] in
-         Hashtbl.iter
-           (fun n _ -> names := `String n :: !names)
-           methods;
-         `Array (List.sort compare !names));
+class cgi () =
+object (self)
+  inherit base
 
-    self#register "system.multicall"
-      (function
-         | [`Array calls] ->
-             `Array
-               (List.map
-                  (function
-                     | `Struct ["methodName", `String name;
-                                "params", `Array params]
-                     | `Struct ["params", `Array params;
-                                "methodName", `String name] ->
-                         (try `Array [(try Hashtbl.find methods name
-                                       with Not_found -> invalid_method name)
-                                        params]
-                          with
-                            | XmlRpc.Error (code, string) ->
-                                `Struct ["faultCode", `Int code;
-                                         "faultString", `String string]
-                            | e ->
-                                `Struct ["faultCode", `Int (-32500);
-                                         "faultString",
-                                         `String
-                                           ("application error. "
-                                            ^ Printexc.to_string e)])
-                     | _ -> invalid_params ())
-                  calls)
-         | _ -> invalid_params ());
-
-  method private process (cgi : Netcgi_types.cgi_activation) =
+  method private process (cgi : Netcgi.cgi) =
     match cgi#request_method with
       | `POST ->
           let input = cgi#argument_value "BODY" in
@@ -100,6 +107,43 @@ object (self)
           cgi#output#commit_work ()
 
   method run () =
+    let config =
+      { Netcgi.default_config with
+          Netcgi_common.permitted_input_content_types = [ "text/xml" ]
+      } in
+
+    let buffered _ ch = new Netchannels.buffered_trans_channel ch in
+    Netcgi_cgi.run ~config ~output_type:(`Transactional buffered) self#process
+end
+
+open Netcgi1_compat
+
+class netplex ?(parallelizer=Netplex_mp.mp()) ?(handler="xmlrpc") () =
+object (self)
+  inherit base
+
+  method private process env (cgi : Netcgi_types.cgi_activation) =
+    match cgi#request_method with
+      | `POST ->
+          let input = cgi#argument_value "BODY" in
+          let output =
+            XmlRpc.serve
+              ~base64_encode ~base64_decode
+              ~datetime_encode ~datetime_decode
+              (fun name ->
+                 try Hashtbl.find methods name
+                 with Not_found -> invalid_method name)
+              input in
+          env#send_output_header ();
+          cgi#output#output_string output;
+          cgi#output#commit_work ()
+      | _ ->
+          env#send_output_header ();
+          cgi#output#output_string
+            "XML-RPC server accepts POST requests only.\n";
+          cgi#output#commit_work ()
+
+  method run () =
     let (opt_list, cmdline_cfg) = Netplex_main.args () in
     Arg.parse
       opt_list
@@ -107,7 +151,7 @@ object (self)
       "usage: netplex [options]";
 
     let xmlrpc =
-      { Nethttpd_services.dyn_handler = (fun _ -> self#process);
+      { Nethttpd_services.dyn_handler = self#process;
         dyn_activation =
           Nethttpd_services.std_activation `Std_activation_buffered;
         dyn_uri = None;
--- trunk/XmlRpcServer.mli
+++ trunk/XmlRpcServer.mli
@@ -1,5 +1,56 @@
 (** XmlRpc Light server. *)
 
+class base :
+object
+  val methods : (string, XmlRpc.value list -> XmlRpc.value) Hashtbl.t
+
+  val mutable base64_encode : string -> string
+  val mutable base64_decode : string -> string
+
+  val mutable datetime_encode :
+      int * int * int * int * int * int * int -> string
+  val mutable datetime_decode :
+      string -> int * int * int * int * int * int * int
+
+  method set_base64_encode : (string -> string) -> unit
+  method set_base64_decode : (string -> string) -> unit
+
+  method set_datetime_encode :
+    (int * int * int * int * int * int * int -> string) -> unit
+  method set_datetime_decode :
+    (string -> int * int * int * int * int * int * int) -> unit
+
+  method register : string -> (XmlRpc.value list -> XmlRpc.value) -> unit
+  method unregister : string -> unit
+end
+
+class cgi :
+  unit ->
+object
+  val methods : (string, XmlRpc.value list -> XmlRpc.value) Hashtbl.t
+
+  val mutable base64_encode : string -> string
+  val mutable base64_decode : string -> string
+
+  val mutable datetime_encode :
+      int * int * int * int * int * int * int -> string
+  val mutable datetime_decode :
+      string -> int * int * int * int * int * int * int
+
+  method set_base64_encode : (string -> string) -> unit
+  method set_base64_decode : (string -> string) -> unit
+
+  method set_datetime_encode :
+    (int * int * int * int * int * int * int -> string) -> unit
+  method set_datetime_decode :
+    (string -> int * int * int * int * int * int * int) -> unit
+
+  method register : string -> (XmlRpc.value list -> XmlRpc.value) -> unit
+  method unregister : string -> unit
+
+  method run : unit -> unit
+end
+
 class netplex :
   ?parallelizer:Netplex_types.parallelizer ->
   ?handler:string ->
