4614ed5ed21da8dd57dc96cf3a8d4bca6c8d286b
dave.benjamin <dave.benjamin@49675f9e-a734-0410-ba66-8ba39d4a960b>
2007-10-15 16:31:48 +0000

    Test cases for messages

--- test/test_message.ml
+++ test/test_message.ml
@@ -17,8 +17,209 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
+let string_of_message = function
+  | XmlRpc.MethodCall (name, params) ->
+      sprintf "MethodCall (%s, [%s])"
+        name (String.concat "; " (List.map XmlRpc.dump params))
+  | XmlRpc.MethodResponse value ->
+      sprintf "MethodResponse (%s)" (XmlRpc.dump value)
+  | XmlRpc.Fault (code, string) ->
+      sprintf "Fault (%d, %s)" code string
+
 let test = "test_message" >:::
   [
+    "of_method_call" >::
+      (fun () ->
+         assert_equal
+           ~printer:Xml.to_string
+           (Xml.Element
+              ("methodCall", [],
+               [Xml.Element ("methodName", [],
+                             [Xml.PCData "demo.addTwoNumbers"]);
+                Xml.Element
+                  ("params", [],
+                   [Xml.Element
+                      ("param", [],
+                       [Xml.Element
+                          ("value", [],
+                           [Xml.Element ("int", [], [Xml.PCData "4"])])]);
+                    Xml.Element
+                      ("param", [],
+                       [Xml.Element
+                          ("value", [],
+                           [Xml.Element ("int", [], [Xml.PCData "5"])])])])]))
+           (XmlRpc.xml_element_of_message
+              (XmlRpc.MethodCall ("demo.addTwoNumbers", [`Int 4; `Int 5]))));
+
+    "to_method_call" >::
+      (fun () ->
+         assert_equal
+           ~printer:string_of_message
+           (XmlRpc.MethodCall ("demo.addTwoNumbers",
+                               [`String "4"; `String "5"]))
+           (XmlRpc.message_of_xml_element
+              (Xml.Element
+                 ("methodCall", [],
+                  [Xml.Element ("methodName", [],
+                                [Xml.PCData "demo.addTwoNumbers"]);
+                   Xml.Element
+                     ("params", [],
+                      [Xml.Element
+                         ("param", [],
+                          [Xml.Element
+                             ("value", [],
+                              [Xml.Element ("string", [],
+                                            [Xml.PCData "4"])])]);
+                       (* test untyped value - should be treated as string *)
+                       Xml.Element
+                         ("param", [],
+                          [Xml.Element
+                             ("value", [], [Xml.PCData "5"])])])]))));
+
+    "of_method_response" >::
+      (fun () ->
+         assert_equal
+           ~printer:Xml.to_string
+           (Xml.Element
+              ("methodResponse", [],
+               [Xml.Element
+                  ("params", [],
+                   [Xml.Element
+                      ("param", [],
+                       [Xml.Element
+                          ("value", [],
+                           [Xml.Element ("string", [],
+                                         [Xml.PCData "test"])])])])]))
+           (XmlRpc.xml_element_of_message
+              (XmlRpc.MethodResponse (`String "test"))));
+
+    "to_method_response" >::
+      (fun () ->
+         assert_equal
+           ~printer:string_of_message
+           (XmlRpc.MethodResponse (`String "test"))
+           (XmlRpc.message_of_xml_element
+              (Xml.Element
+                 ("methodResponse", [],
+                  [Xml.Element
+                     ("params", [],
+                      [Xml.Element
+                         ("param", [],
+                          [Xml.Element
+                             ("value", [],
+                              [Xml.Element ("string", [],
+                                            [Xml.PCData "test"])])])])])));
+         assert_equal
+           ~printer:string_of_message
+           (XmlRpc.MethodResponse (`String "test"))
+           (XmlRpc.message_of_xml_element
+              (Xml.Element
+                 ("methodResponse", [],
+                  [Xml.Element
+                     ("params", [],
+                      [Xml.Element
+                         ("param", [],
+                          (* test untyped value *)
+                          [Xml.Element
+                             ("value", [], [Xml.PCData "test"])])])]))));
+
+    "of_fault" >::
+      (fun () ->
+         assert_equal
+           ~printer:Xml.to_string
+           (Xml.Element
+              ("methodResponse", [],
+               [Xml.Element
+                  ("fault", [],
+                   [Xml.Element
+                      ("value", [],
+                       [Xml.Element
+                          ("struct", [],
+                           [Xml.Element
+                              ("member", [],
+                               [Xml.Element ("name", [],
+                                             [Xml.PCData "faultCode"]);
+                                Xml.Element
+                                  ("value", [],
+                                   [Xml.Element ("int", [],
+                                                 [Xml.PCData "12345"])])]);
+                            Xml.Element
+                              ("member", [],
+                               [Xml.Element ("name", [],
+                                             [Xml.PCData "faultString"]);
+                                Xml.Element
+                                  ("value", [],
+                                   [Xml.Element
+                                      ("string", [],
+                                       [Xml.PCData
+                                          "something terrible happened"
+                                       ])])])])])])]))
+           (XmlRpc.xml_element_of_message
+              (XmlRpc.Fault (12345, "something terrible happened"))));
+
+    "to_fault" >::
+      (fun () ->
+         assert_equal
+           ~printer:string_of_message
+           (XmlRpc.Fault (12345, "something terrible happened"))
+           (XmlRpc.message_of_xml_element
+              (Xml.Element
+                 ("methodResponse", [],
+                  [Xml.Element
+                     ("fault", [],
+                      [Xml.Element
+                         ("value", [],
+                          [Xml.Element
+                             ("struct", [],
+                              [Xml.Element
+                                 ("member", [],
+                                  [Xml.Element ("name", [],
+                                                [Xml.PCData "faultCode"]);
+                                   Xml.Element
+                                     ("value", [],
+                                      [Xml.Element ("int", [],
+                                                    [Xml.PCData "12345"])])]);
+                               Xml.Element
+                                 ("member", [],
+                                  [Xml.Element ("name", [],
+                                                [Xml.PCData "faultString"]);
+                                   Xml.Element
+                                     ("value", [],
+                                      [Xml.Element
+                                         ("string", [],
+                                          [Xml.PCData
+                                             "something terrible happened"
+                                          ])])])])])])])));
+         assert_equal
+           ~printer:string_of_message
+           (XmlRpc.Fault (12345, "something terrible happened"))
+           (XmlRpc.message_of_xml_element
+              (Xml.Element
+                 ("methodResponse", [],
+                  [Xml.Element
+                     ("fault", [],
+                      [Xml.Element
+                         ("value", [],
+                          [Xml.Element
+                             ("struct", [],
+                              [Xml.Element
+                                 ("member", [],
+                                  [Xml.Element ("name", [],
+                                                [Xml.PCData "faultCode"]);
+                                   Xml.Element
+                                     ("value", [],
+                                      [Xml.Element ("int", [],
+                                                    [Xml.PCData "12345"])])]);
+                               Xml.Element
+                                 ("member", [],
+                                  [Xml.Element ("name", [],
+                                                [Xml.PCData "faultString"]);
+                                   (* test untyped value *)
+                                   Xml.Element
+                                     ("value", [],
+                                      [Xml.PCData
+                                         "something terrible happened"
+                                      ])])])])])]))));
   ]
 
 let tests = test :: tests
