a192567f87aa961c98d8e3730bfc3afe0f6ddd49
dave.benjamin <dave.benjamin@49675f9e-a734-0410-ba66-8ba39d4a960b>
2007-11-03 19:05:03 +0000

    Documentation comments for XmlRpcDateTime

--- trunk/Makefile
+++ trunk/Makefile
@@ -5,7 +5,7 @@ SOURCES = XmlRpcBase64.mli XmlRpcBase64.ml XmlRpcDateTime.mli XmlRpcDateTime.ml 
 PACKS = xml-light,netclient,nethttpd-for-netcgi2
 LIBINSTALL_FILES = XmlRpcBase64.mli XmlRpcBase64.cmi XmlRpcDateTime.mli XmlRpcDateTime.cmi XmlRpc.mli XmlRpc.cmi XmlRpcServer.mli XmlRpcServer.cmi xmlrpc-light.cma xmlrpc-light.cmxa xmlrpc-light.a
 OCAMLDOC = ocamlfind ocamldoc -package xml-light,nethttpd-for-netcgi2
-DOC_FILES = XmlRpc.mli XmlRpcServer.mli XmlRpcBase64.mli XmlRpcDateTime.mli
+DOC_FILES = XmlRpc.mli XmlRpcServer.mli XmlRpcDateTime.mli XmlRpcBase64.mli
 
 all: native-code-library byte-code-library
 install: libinstall
--- trunk/XmlRpcDateTime.mli
+++ trunk/XmlRpcDateTime.mli
@@ -17,29 +17,81 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
+(** Date/time type. *)
+
+(** {6 Types} *)
+
+(** Raised by {!of_string} if a string could not be parsed. The exception
+    contains the input string. *)
 exception Parse_error of string
 
+(** Type of XmlRpc-compatible date/time values.
+    (year, month, day, hour, minute, second, time zone offset in minutes) *)
 type t = (int * int * int * int * int * int * int)
 
+(** {6 Comparison} *)
+
+(** Standard comparator for date/time values. Converts all values to UTC
+    before comparing to ensure correct behavior with values of differing
+    time zones. *)
 val compare : t -> t -> int
+
+(** Standard equality function for date/time values. Converts all values
+    to UTC before comparing. *)
 val equal : t -> t -> bool
+
+(** Standard hash function for date/time values. Converts values to UTC
+    before hashing. *)
 val hash : t -> int
 
+(** {6 Current date and time} *)
+
+(** Returns the current date and time in the local time zone. *)
 val now : unit -> t
+
+(** Returns the current date and time in UTC. *)
 val now_utc : unit -> t
 
+(** {6 Time zone adjustments} *)
+
+(** Adjusts the time zone offset, preserving equality. *) 
 val set_tz_offset : int -> t -> t
+
+(** Forces the time zone offset to a different value, ignoring all other
+    fields. Use this to correct the time zone of a date/time value that
+    was received without a time zone offset and is known not to be UTC. *)
 val fix_tz_offset : int -> t -> t
 
+(** {6 Conversion} *)
+
+(** Builds a date/time value from epoch seconds in the local time zone. *)
 val from_unixfloat : float -> t
+
+(** Builds a date/time value from epoch seconds in UTC. *)
 val from_unixfloat_utc : float -> t
+
+(** Converts a date/time value to epoch seconds in the local time zone. *)
 val to_unixfloat : t -> float
+
+(** Converts a date/time value to epoch seconds in UTC. *)
 val to_unixfloat_utc : t -> float
 
+(** Builds a date/time value from a Unix.tm value in the local time zone. *)
 val from_unixtm : Unix.tm -> t
+
+(** Builds a date/time value from a Unix.tm value in UTC. *)
 val from_unixtm_utc : Unix.tm -> t
+
+(** Converts a date/time value to a Unix.tm value in the local time zone. *)
 val to_unixtm : t -> Unix.tm
+
+(** Converts a date/time value to a Unix.tm value in UTC. *)
 val to_unixtm_utc : t -> Unix.tm
 
+(** {6 ISO-8601 parsing and generation} *)
+
+(** Parses an (XmlRpc-flavor) ISO-8601 date/time value from a string. *)
 val of_string : string -> t
+
+(** Generates an ISO-8601 string from a date/time value. *)
 val to_string : t -> string
