be78ddf48c319a5521c02837f673e1507c4f27e1
dave.benjamin <dave.benjamin@49675f9e-a734-0410-ba66-8ba39d4a960b>
2008-07-06 18:13:17 +0000

    Add get_page_templates method

--- trunk/examples/wordpress/WordPress.ml
+++ trunk/examples/wordpress/WordPress.ml
@@ -415,6 +415,11 @@ object (self)
   method get_page_list () =
     map_array PageListItem.of_xmlrpc (rpc#call "wp.getPageList" std_args)
 
+  method get_page_templates () =
+    match rpc#call "wp.getPageTemplates" std_args with
+      | `Struct pairs -> List.map (fun (k, v) -> (k, XmlRpc.dump v)) pairs
+      | other -> raise (Type_error (XmlRpc.dump other))
+
   method new_page content publish =
     int_value
       (rpc#call "wp.newPage"
