af63cda442a1a22bf22b83b6ac8cacb940cdb236
dave.benjamin <dave.benjamin@49675f9e-a734-0410-ba66-8ba39d4a960b>
2008-07-06 18:13:17 +0000

    Add get_page_templates method

--- examples/wordpress/WordPress.mli
+++ examples/wordpress/WordPress.mli
@@ -167,6 +167,7 @@ object
   method get_categories : unit -> Category.t list
   method get_page : int -> Page.t
   method get_page_list : unit -> PageListItem.t list
+  method get_page_templates : unit -> (string * string) list
   method get_pages : unit -> Page.t list
   method get_post : int -> Post.t
   method get_recent_posts : int -> Post.t list
