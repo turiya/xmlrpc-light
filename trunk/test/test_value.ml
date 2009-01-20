6ab93bd2acf4afe8c871e3c6ba6143afd8ec4823
dave.benjamin <dave.benjamin@49675f9e-a734-0410-ba66-8ba39d4a960b>
2009-01-20 00:34:32 +0000

    Update copyright dates

--- trunk/test/test_value.ml
+++ trunk/test/test_value.ml
@@ -1,6 +1,6 @@
 (*
  * XmlRpc Light, a small XmlRpc library based on Xml Light and Ocamlnet
- * Copyright (C) 2007 Dave Benjamin (dave@ramenlabs.com)
+ * Copyright (C) 2007-2009 Dave Benjamin (dave@ramenlabs.com)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
